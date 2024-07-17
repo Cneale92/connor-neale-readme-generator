10b9fbd4d4c85227c2b738ef0347e2fe7b902efe
Cneale92 <connorneale92@gmail.com>
2024-07-16 21:40:54 -0500

    added the array of questions for user input

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  `What is your GitHub username?`,
+  `What is your email address?`,
+  `What is the your project's name?`,
+  `Please write a short description about your project:`,
+  `What kind of license should your project have?`,
+  `What command should be run to install dependencies?`,
+  `What command should be run to run tests?`,
+  `What does the user need to know about using the repo?`,
+  `What does the user need to know about contributing to the repo?`,
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
